840cf3205a3b1303c1f56dab2345f76246afa16d
Thembani Maswanganyi <t.maswanganyi101@gmail.com>
2023-09-18 15:55:03 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,58 @@
+import streamlit as st
+import pandas as pd
+from streamlit_option_menu import option_menu
+from streamlit_lottie import st_lottie
+import requests
+
+def load_lottie_url(url):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+lottie_coding = load_lottie_url("https://assets9.lottiefiles.com/packages/lf20_vnikrcia.json")
+lottie_learn = load_lottie_url("https://assets5.lottiefiles.com/packages/lf20_knvn3kk2.json")
+lottie_chart = load_lottie_url("https://assets5.lottiefiles.com/packages/lf20_q5qeoo3q.json")
+
+def main():
+
+    selected = option_menu(
+        menu_title=None,
+        options=['Home','Dashboard','Contact'],
+        icons=['house','bar-chart','envelope'],
+        default_index=0,
+        orientation='horizontal'
+    )
+    
+    if selected == 'Home':
+        from PIL import Image
+        image = Image.open('assets/img/data-analysis-charts-760.png')
+        
+        st.markdown("<h1 style='text-align: center;'>Tief Everything!!!</h1>", unsafe_allow_html=True)
+
+        left_column, right_column = st.columns(2)
+        with left_column:
+             st.image(image, caption='Tief Everything')
+        with right_column:
+             st.write('A streamlit app to analyse user dataset')
+             st.write('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum')
+
+        st.write("---")
+        left_column, right_column = st.columns(2)
+        with left_column:
+            st_lottie(lottie_coding)
+        with right_column:
+            st_lottie(lottie_learn)
+
+    if selected == 'Dashboard':
+        st.write('Dashboard')
+
+    if selected == 'Contact':
+         st.write('Contact')
+
+
+
+
+# Required to let Streamlit instantiate our web app.  
+if __name__ == '__main__':
+	main()
